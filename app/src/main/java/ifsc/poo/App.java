a9179af3116a7fc6499ec9389d61a174d4caba4e
Kauana-bm24 <kauana.bm24@aluno.ifsc.edu.br>
2025-05-15 23:41:31 +0000

    adicionado barco e grade

--- app/src/main/java/ifsc/poo/App.java
+++ app/src/main/java/ifsc/poo/App.java
@@ -1,26 +1,20 @@
 package ifsc.poo;
 import edu.princeton.cs.algs4.Draw;
 
-
-
 public class App {
    // Atributo
-   private final Draw draw;
-
+   private Draw draw;
 
    // Construtor
    public App() {
        this.draw = new Draw();
        this.draw.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 
-
        // Ajusta o tamanho da tela
        this.draw.setCanvasSize(1000, 600); //Largura e altura
 
-
        // Ajusta a escala X
        this.draw.setXscale(0, 1000); //Define a escala X
-
 
        // Ajusta a escala Y
        this.draw.setYscale(0, 600); //Define a escala Y
@@ -36,9 +30,6 @@ public class App {
    // Main
    public static void main(String[] args) {
        App app = new App();
-       app.draw.setVisible(true);
+       Draw.setVisible(true);
    }
 }
-
-
-
---
+++ app/src/main/java/ifsc/poo/barco.java
@@ -0,0 +1,46 @@
+package ifsc.poo;//barco
+import edu.princeton.cs.algs4.Draw;
+
+
+public class Barco {//Atributos/variaveis
+   private Grade grade;
+   private int tamanho;
+   private int posX, posY;
+   private boolean vertical;
+   private static final int tamanho2 = 40;
+   public int X; int Y;
+      
+  
+
+
+   public Barco(Grade grade, int tamanho, int posX, int posY, boolean vertical) {//metodo
+       this.grade = grade;
+       this.tamanho = tamanho;
+       this.posX = posX;
+       this.posY = posY;
+       this.vertical = vertical;
+   }
+
+
+   public void desenhar(Draw draw) {
+       draw.setPenRadius();
+       draw.setPenColor(Draw.BLUE);//Seta a cor
+
+
+       for (int i = 0; i < tamanho; i++) {
+           int xPixel = grade.getX() + (posX * tamanho2);
+           int yPixel = grade.getY() + (posY * tamanho2);
+
+
+           if (vertical) {
+               yPixel += i * tamanho2;
+           } else {
+               xPixel += i * tamanho2;
+           }
+
+
+           draw.filledRectangle(xPixel + tamanho2 / 2.0, yPixel + tamanho2 / 2.0,
+                                tamanho2 / 2.0, tamanho2 / 2.0);
+       }
+   }
+}
