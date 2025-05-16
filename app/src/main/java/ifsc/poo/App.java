440566f42e42a1e2731cef8b09fca337b1109d4d
unknown <kauana.bm24@aluno.ifsc.edu.br>
2025-05-16 00:39:34 -0300

    exercicio 4

--- app/src/main/java/ifsc/poo/App.java
+++ app/src/main/java/ifsc/poo/App.java
@@ -30,22 +30,24 @@ public class App {
         Grade grade = new Grade(50, 50);
         grade.desenhar(app.draw);
 
-        Barco barco = new Barco(grade, 4, 0, 0, true);
+        Grade grade1 = new Grade(500, 50);
+        grade1.desenhar(app.draw);
+
+        Barco barco = new Barco(grade, 3, 0, 0, true);
         barco.desenhar(app.draw);
         // Desenha os barcos
-        Barco barco1 = new Barco(grade,2,0,8,true);
+        Barco barco1 = new Barco(grade,2,0,8,false);
         barco1.desenhar(app.draw);
 
         Barco barco2 = new Barco(grade,3,5,0,true);
         barco2.desenhar(app.draw);
 
-        Barco barco3 = new Barco(grade,4,4,5,false);
+        Barco barco3 = new Barco(grade,4,2,4,false);
         barco3.desenhar(app.draw);
 
-        Barco barco4 = new Barco(grade,4,7,2,true);
+        Barco barco4 = new Barco(grade,5,9,5,true);
         barco4.desenhar(app.draw);
 
-        Barco barco5 = new Barco(grade,5,2,7,false);
-        barco5.desenhar(app.draw);
+
     }
 }
